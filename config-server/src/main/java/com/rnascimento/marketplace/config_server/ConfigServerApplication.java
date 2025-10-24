5c024ae95078a10fb97e29c63c6b3832c822ea30
Rafael Nascimento <rnascimentoo@live.com>
2025-10-23 22:48:01 -0300

    chore: atualização do projeto config-server

--- config-server/src/main/java/com/rnascimento/marketplace/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/rnascimento/marketplace/config_server/ConfigServerApplication.java
@@ -2,12 +2,16 @@ package com.rnascimento.marketplace.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 
 }
