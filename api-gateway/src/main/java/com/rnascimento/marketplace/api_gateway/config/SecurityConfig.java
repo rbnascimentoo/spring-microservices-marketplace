5eadcf3cdc8804444b8ad130e2c8fb2b999617ae
rafael-nascimento <rafael.nascimento@prodeb.ba.gov.br>
2026-03-11 17:34:00 -0300

    ajustes

---
+++ api-gateway/src/main/java/com/rnascimento/marketplace/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.rnascimento.marketplace.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        // actuator liberado
+                        .pathMatchers("/actuator/**").permitAll()
+                        // login/auth
+                        .pathMatchers("/auth/**").permitAll()
+                        // resto precisa de token
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+                .build();
+    }
+}
